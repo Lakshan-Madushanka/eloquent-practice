2128f14d8d8ae9a3584ba17647b92803d8c58a81
Lakshan Madushanka <epmadushanka@gmail.com>
2022-03-08 12:49:19 +0530

    Back End added

---
+++ README.md
@@ -0,0 +1,2 @@
+## Laracast post that include the problem which is solved by this repo.
+https://laracasts.com/discuss/channels/laravel/group-by-month-and-company-sum-count
---
+++ tests/Feature/JoBCardTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Database\Seeders\JobCardSeeder;
+use Doctrine\DBAL\Query\QueryException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class JoBCardTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $seeder = JobCardSeeder::class;
+
+    public function test_job_analysis_report_return_accurate_data()
+    {
+        try {
+            DB::reconnect();
+        } catch (QueryException $exception) {
+            echo $exception;
+        }
+
+
+        $response = $this->json('get', route('job_analysis'));
+
+        $response->assertJson(function (AssertableJson $json) {
+            $json->hasAll('jobsArray', 'jobCodes')
+                ->has('jobsArray', 3, function (AssertableJson $json) {
+                    $json->where('A.count', 2);
+                    $json->where('A.amount', 110);
+                })
+                ->has('jobCodes', 2)
+                ->etc();
+        });
+
+        $response->assertStatus(200);
+    }
+}
